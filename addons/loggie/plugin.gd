25e73f0ccb7f9175acf709986d33bbbcdc55265b
Kris <kris.cu@hotmail.com>
2024-09-30 10:49:55 +0200

    Update the 'doc' entry for each project setting and add a comment.

--- addons/loggie/plugin.gd
+++ addons/loggie/plugin.gd
@@ -17,7 +17,7 @@ const project_settings = {
 		"type" : TYPE_STRING,
 		"hint" : PROPERTY_HINT_TYPE_STRING,
 		"hint_string" : "e.g. res://addons/loggie/custom_loggie_settings.gd",
-		"doc" : "The path to a custom .gd script that a valid LoggieSettings (or an extension of it) class can be instantiated from."
+		"doc" : "The path to a custom .gd script that a valid LoggieSettings class instance (or an extension of that class) can be instantiated from."
 	},
 	"terminal_mode" = {
 		"path": "loggie/general/terminal_mode",
@@ -25,7 +25,7 @@ const project_settings = {
 		"type" : TYPE_INT,
 		"hint" : PROPERTY_HINT_ENUM,
 		"hint_string" : "Plain:0,ANSI:1,BBCode:2",
-		"doc" : "Set the terminal mode.",
+		"doc" : "Choose the terminal for which loggie should preprocess the output so that it displays as intended.[br][br]Use BBCode for Godot console.[br]Use ANSI for Powershell, Bash, etc.[br]Use PLAIN for log files.",
 	},
 	"log_level" = {
 		"path": "loggie/general/log_level",
@@ -33,7 +33,7 @@ const project_settings = {
 		"type" : TYPE_INT,
 		"hint" : PROPERTY_HINT_ENUM,
 		"hint_string" : "Error:0,Warn:1,Notice:2,Info:3,Debug:4",
-		"doc" : "",
+		"doc" : "Choose the level of messages which should be displayed. Loggie displays all messages that are outputted at the currently set level (or any lower level).",
 	},
 	"show_system_specs" = {
 		"path": "loggie/general/show_system_specs",
@@ -41,7 +41,7 @@ const project_settings = {
 		"type" : TYPE_BOOL,
 		"hint" : PROPERTY_HINT_NONE,
 		"hint_string" : "",
-		"doc" : "",
+		"doc" : "Should Loggie log the system and device specs of the user as soon as it is booted?",
 	},
 	"output_timestamps" = {
 		"path": "loggie/timestamps/output_timestamps",
@@ -49,7 +49,7 @@ const project_settings = {
 		"type" : TYPE_BOOL,
 		"hint" : PROPERTY_HINT_NONE,
 		"hint_string" : "",
-		"doc" : "",
+		"doc" : "Should Loggie output a timestamp prefix with each message, showing the exact moment when that log line was produced?",
 	},
 	"timestamps_use_utc" = {
 		"path": "loggie/timestamps/timestamps_use_utc",
@@ -57,7 +57,7 @@ const project_settings = {
 		"type" : TYPE_BOOL,
 		"hint" : PROPERTY_HINT_NONE,
 		"hint_string" : "",
-		"doc" : "",
+		"doc" : "If 'Output Timestamps' is true, should those timestamps use the UTC time. If not, local system time is used instead.",
 	},
 	"output_errors_to_console" = {
 		"path": "loggie/preprocessing/output_errors_also_to_console",
@@ -65,7 +65,7 @@ const project_settings = {
 		"type" : TYPE_BOOL,
 		"hint" : PROPERTY_HINT_NONE,
 		"hint_string" : "",
-		"doc" : "",
+		"doc" : "If true, errors printed by Loggie will also be visible through an additional print in the main output.",
 	},
 	"output_warnings_to_console" = {
 		"path": "loggie/preprocessing/output_warnings_also_to_console",
@@ -73,7 +73,7 @@ const project_settings = {
 		"type" : TYPE_BOOL,
 		"hint" : PROPERTY_HINT_NONE,
 		"hint_string" : "",
-		"doc" : "",
+		"doc" : "If true, warnings printed by Loggie will also be visible through an additional print in the main output.",
 	},
 	"use_print_debug_for_debug_msgs" = {
 		"path": "loggie/preprocessing/use_print_debug_for_debug_msgs",
@@ -81,7 +81,7 @@ const project_settings = {
 		"type" : TYPE_BOOL,
 		"hint" : PROPERTY_HINT_NONE,
 		"hint_string" : "",
-		"doc" : "",
+		"doc" : "If true, 'debug' level messages outputted by Loggie will be printed using Godot's 'print_debug' function, which is more verbose.",
 	},
 	"derive_and_display_class_names_from_scripts" = {
 		"path": "loggie/preprocessing/derive_and_display_class_names_from_scripts",
@@ -89,7 +89,7 @@ const project_settings = {
 		"type" : TYPE_BOOL,
 		"hint" : PROPERTY_HINT_NONE,
 		"hint_string" : "",
-		"doc" : "",
+		"doc" : "If true, Loggie will attempt to find out the name of the main class from which the log line is coming and append it in front of the message.",
 	},
 	"output_message_domain" = {
 		"path": "loggie/preprocessing/output_message_domain",
@@ -97,7 +97,7 @@ const project_settings = {
 		"type" : TYPE_BOOL,
 		"hint" : PROPERTY_HINT_NONE,
 		"hint_string" : "",
-		"doc" : "",
+		"doc" : "If true, logged messages will have the domain they are coming from prepended to them.",
 	},
 	"box_characters_mode" = {
 		"path": "loggie/preprocessing/box_characters_mode",
@@ -105,7 +105,7 @@ const project_settings = {
 		"type" : TYPE_INT,
 		"hint" : PROPERTY_HINT_ENUM,
 		"hint_string" : "Compatible:0,Pretty:1",
-		"doc" : "",
+		"doc" : "There are two sets of box characters defined in LoggieSettings - one set contains prettier characters that produce a nicer looking box, but may not render correctly in the context of various terminals. The other set contains characters that produce a less pretty box, but are compatible with being shown in most terminals.",
 	}
 }
 
@@ -133,6 +133,8 @@ func remove_loggie_project_setings():
 		push_error("Loggie - Encountered error %d while saving project settings." % error)
 
 ## Adds a new project setting to Godot.
+## TODO: Figure out how to also add the documentation to the ProjectSetting so that it shows up in the Godot Editor tooltip
+## when the setting is hovered over.
 func add_project_setting(setting_name: String, default_value : Variant, value_type: int, type_hint: int = PROPERTY_HINT_NONE, hint_string: String = "", documentation : String = ""):
 	if ProjectSettings.has_setting(setting_name): 
 		return
@@ -143,8 +145,7 @@ func add_project_setting(setting_name: String, default_value : Variant, value_ty
 		"name": setting_name,
 		"type": value_type,
 		"hint": type_hint,
-		"hint_string": hint_string,
-		"doc" : documentation
+		"hint_string": hint_string
 	})
 
 	ProjectSettings.set_initial_value(setting_name, default_value)
