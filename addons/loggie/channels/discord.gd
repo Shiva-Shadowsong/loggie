e0a2084b8c9a527dabb30111a103b7e5674589ab
Kris <kris.cu@hotmail.com>
2024-12-05 14:30:02 +0100

    Update the Discord channel skeleton to follow new class conventions.

--- addons/loggie/channels/discord.gd
+++ addons/loggie/channels/discord.gd
@@ -1,4 +1,8 @@
-class_name TerminalChannel extends LoggieMsgChannel
+class_name DiscordLoggieMsgChannel extends LoggieMsgChannel
 
-func dispatch(message: LoggieMsg):
-	print(message.message)
+func _init() -> void:
+	self.ID = "discord"
+	self.preprocess_flags = 0 # For this type of channel, this will be applied dynamically by Loggie after it loads LoggieSettings.
+
+func send(msg : LoggieMsg, msg_type : LoggieEnums.MsgType):
+	pass
