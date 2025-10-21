5a6cf4c5918afb5c3089d6510505d42d2d1222c9
Kris <kris.cu@hotmail.com>
2025-10-21 17:19:10 +0200

    Implement LoggieTestCase class.

---
+++ test/LoggieTestCase.gd
@@ -0,0 +1,60 @@
+class_name LoggieTestCase extends Node
+
+signal finished()
+
+var result : Result = Result.DidntRun
+
+var settings : LoggieSettings = LoggieSettings.new()
+
+var _original_settings : LoggieSettings
+
+var _timeout_counter : Timer = Timer.new()
+
+enum Result {
+	Fail,
+	Success,
+	DidntRun,
+	TimedOut
+}
+
+func _init() -> void:
+	_original_settings = Loggie.settings
+
+## Override this function in your test case to define how it runs.
+func run():
+	pass
+
+func success():
+	result = Result.Success
+	_finish()
+
+func fail():
+	result = Result.Fail
+	_finish()
+	
+func _run_timeout_counter() -> void:
+	if not get_children().has(_timeout_counter):
+		_timeout_counter.name = "TimeoutCounter"
+		_timeout_counter.wait_time = 5.0
+		_timeout_counter.one_shot = true
+		_timeout_counter.timeout.connect(func():
+			result = Result.TimedOut
+			finished.emit()
+		)
+		add_child(_timeout_counter)
+	_timeout_counter.start(5.0)
+
+func _finish():
+	var msg : String = "?"
+	match self.result:
+		Result.Success:
+			msg = "[color=PALE_GREEN]✔️ Passed[/color]"
+		Result.Fail:
+			msg = "[color=SALMON]❌ Failed[/color]"
+		Result.DidntRun:
+			msg = "[color=GOLD]☢️ Didn't run[/color]"
+	
+	msg = "[b]{name}[/b]: {msg}".format({"name": get_script().get_global_name(), "msg": msg})
+	print_rich(msg)
+	Loggie.settings = _original_settings
+	finished.emit()
