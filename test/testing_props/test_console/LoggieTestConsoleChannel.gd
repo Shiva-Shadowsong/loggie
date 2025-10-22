44a85a976180e3c3e8be5b921eec185f735d2c84
Kris <kris.cu@hotmail.com>
2025-10-22 16:22:38 +0200

    LoggieTestConsoleChannel now starts out with preprocess_flags = 0 and has a method for resetting its settings.

--- test/testing_props/test_console/LoggieTestConsoleChannel.gd
+++ test/testing_props/test_console/LoggieTestConsoleChannel.gd
@@ -5,7 +5,7 @@ var console : LoggieTestConsole
 
 func _init() -> void:
 	ID = "test_console"
-	preprocess_flags = LoggieEnums.PreprocessStep.APPEND_TIMESTAMPS | LoggieEnums.PreprocessStep.APPEND_DOMAIN_NAME | LoggieEnums.PreprocessStep.APPEND_CLASS_NAME
+	preprocess_flags = 0
 
 func send(msg : LoggieMsg, _msg_type : LoggieEnums.MsgType):
 	if not is_instance_valid(console):
@@ -15,3 +15,6 @@ func send(msg : LoggieMsg, _msg_type : LoggieEnums.MsgType):
 	var loggie = msg.get_logger()
 	var text = LoggieTools.convert_string_to_format_mode(msg.last_preprocess_result, loggie.settings.msg_format_mode)
 	console.add_text(text)
+
+func reset():
+	preprocess_flags = 0
